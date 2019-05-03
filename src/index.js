177cd8c664ecebe9c964e06a6dfaec9f16db9f4f
Sergey Kluchkovsky <kaineer@gmail.com>
2019-05-03 09:29:01 +0500

    Добавляет тестовые данные

--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import {App} from './components/app/app';
 
+const places = [
+  {
+    title: `Beautiful & luxurious apartment at great location`,
+  },
+  {
+    title: `Wood and stone place`,
+  },
+  {
+    title: `Strange and abandoned grove`,
+  },
+  {
+    title: `Winter stronghold, just for you`
+  }
+];
+
 const init = () => {
   ReactDOM.render(
-    <App/>,
-    document.querySelector('#root')
+      <App places={places} />,
+      document.querySelector(`#root`)
   );
 };
 
