63889e070433892d93b83828835cdbebe2f03e8b
Sergey Kluchkovsky <kaineer@gmail.com>
2019-05-03 10:54:51 +0500

    Добавляет e2e тест для place

---
+++ src/components/place/place.e2e.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import {Place} from './place';
+
+Enzyme.configure({adapter: new Adapter()});
+
+describe(`Place e2e`, () => {
+  let clickHandler;
+  let place;
+  let title;
+
+  beforeEach(() => {
+    clickHandler = jest.fn();
+    place = shallow(
+        <Place
+          title={``}
+          onClick={clickHandler}
+        />
+    );
+    title = place.find(`.place-card__name a`);
+  });
+
+  it(`should have a title`, () => {
+    expect(title).toHaveLength(1);
+  });
+
+  describe(`before click`, () => {
+    it(`should not call clickHandler`, () => {
+      expect(clickHandler).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  describe(`after click`, () => {
+    beforeEach(() => {
+      title.simulate(`click`);
+    });
+
+    it(`should call clickHandler once`, () => {
+      expect(clickHandler).toHaveBeenCalledTimes(1);
+    });
+  });
+});
