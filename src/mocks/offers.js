eea79ab5a924b52b526f85e9cd55be634ae6966c
Sergey Kluchkovsky <kaineer@gmail.com>
2019-05-08 10:21:02 +0500

    Добавляет тестовые данные

---
+++ src/mocks/offers.js
@@ -0,0 +1,32 @@
+// src/mocks/offers.js
+
+module.exports = [
+  {
+    title: `Beautiful & luxurious apartment at great location`,
+    price: 120,
+    grade: 4.5,
+    isPremium: true,
+    type: 'Apartment',
+  },
+  {
+    title: `Wood and stone place`,
+    price: 60,
+    grade: 2,
+    isPremium: false,
+    type: 'Place',
+  },
+  {
+    title: `Strange and abandoned grove`,
+    price: 70,
+    grade: 2,
+    isPremium: false,
+    type: 'Environment',
+  },
+  {
+    title: `Winter stronghold, just for you`,
+    price: 300,
+    grade: 5,
+    isPremium: true,
+    type: 'Castle',
+  }
+];
