0b7112b652c40d994fe06dc5afb55e2bc0bcc1e4
Sergey Kluchkovsky <kaineer@gmail.com>
2019-05-08 11:21:47 +0500

    Выносит e2e-конфигурацию в отдельный файл

---
+++ config/jest.config.js
@@ -0,0 +1,19 @@
+// config/jest.config.js
+
+const {join} = require('path');
+
+module.exports = {
+  rootDir: join(__dirname, '..'),
+  roots: [
+    '<rootDir>/src/components'
+  ],
+  setupFiles: [
+    join(__dirname, 'jest/enzyme.js')
+  ],
+  moduleDirectories: ["node_modules", "src"],
+  testMatch: [
+    '**/*.test.js',
+    '**/*.e2e.test.js',
+  ],
+  verbose: false
+};
