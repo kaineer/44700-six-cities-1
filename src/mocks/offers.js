928a1014ade0323965053b60be79375bc7d874c4
Sergey Kluchkovsky <kaineer@gmail.com>
2019-05-15 18:20:30 +0500

    Добавляет координаты

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -8,6 +8,7 @@ module.exports = [
     isPremium: true,
     picture: `img/apartment-01.jpg`,
     type: `Apartment`,
+    location: [52.3909553943508, 4.85309666406198],
   },
   {
     title: `Wood and stone place`,
@@ -16,6 +17,7 @@ module.exports = [
     isPremium: false,
     picture: `img/apartment-02.jpg`,
     type: `Place`,
+    location: [52.369553943508, 4.85309666406198],
   },
   {
     title: `Strange and abandoned grove`,
@@ -24,6 +26,7 @@ module.exports = [
     isPremium: false,
     picture: `img/apartment-03.jpg`,
     type: `Environment`,
+    location: [52.3909553943508, 4.929309666406198],
   },
   {
     title: `Winter stronghold, just for you`,
@@ -32,5 +35,6 @@ module.exports = [
     isPremium: true,
     picture: `img/apartment-01.jpg`,
     type: `Castle`,
+    location: [52.3809553943508, 4.939309666406198],
   }
 ];
